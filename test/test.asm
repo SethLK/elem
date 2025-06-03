1782d3cd3d9307a79a86d052d64089b5d822160a
Seth Linn Khant <sethlk2006@gmail.com>
2025-06-03 15:29:14 +0630

    Delete test

--- test/test.asm
+++
@@ -1,8 +0,0 @@
-global _start
-
-section .text
-
-_start:
-    mov rax, 60         ; System call number for exit
-    xor rdi, 2          ; Exit code (0)
-    syscall             ; Make the system call
